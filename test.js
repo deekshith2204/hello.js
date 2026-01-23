00c6dbb91da157e7e125ce977c55c6b0f2733ed9
deekshith2204 <162458822+deekshith2204@users.noreply.github.com>
2026-01-23 18:58:55 +0530

    moved from github/dev patch

--- test.js
+++ test.js
@@ -1,1 +1,13 @@
 alert("Hello World!");
+function summn(m,f){
+    let sum=0;
+    let i;
+    for(i=0,i<f.length,i++){
+        if(m[i]%f[i]==0){
+            sum+=m[i];
+        }
+    }
+    return sum
+
+}
+console.log(summn([3,5],[1,2,3,4,5,6,7,8,9]))
