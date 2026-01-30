5c1ebd8e079cb5cbf541873919e095ff0d634386
deekshith2204 <162458822+deekshith2204@users.noreply.github.com>
2026-01-30 18:34:57 +0530

    errors fixed

--- test.js
+++ test.js
@@ -1,16 +1,13 @@
-alert("Hello World!");
-function summn(m, f) {
+function summn(f,m) {
     let sum = 0;
     for (let i of m) {
-        for (let j !=0 &&  j of f) {
-            if (m[i] % f[j] == 0) {
-                sum += m[i];
+        for (let j of f) {
+            if (i % j == 0) {
+                sum += i;
                 break;
             }
         }
     }
+    return sum
 }
-return sum
-
-}
-console.log(summn([3, 5], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
+console.log(summn([2,3], [4, 5, 6, 7, 8, 9]))
