20fff226a8a8166577146be432751567404906eb
deekshith2204 <162458822+deekshith2204@users.noreply.github.com>
2026-01-30 18:20:19 +0530

    errors fixed

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ function summn(m, f) {
     let sum = 0;
     let i;
     let j;
-    for (i = 0; i < f.length; i++) {
-        for (j = 0; j < m.length; j++) {
+    for (i = 0; i < m.length; i++) {
+        for (j = 0; j < f.length; j++) {
             if (m[i] % f[j] == 0) {
                 sum += m[i];
                 break;
