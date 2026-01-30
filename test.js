21d66ed1486a7e1b59ac2a843d9ddd0ae2b99932
deekshith2204 <162458822+deekshith2204@users.noreply.github.com>
2026-01-30 18:09:51 +0530

    error commited fix

--- test.js
+++ test.js
@@ -3,11 +3,10 @@ function summn(m,f){
     let sum=0;
     let i;
     for(i=0;i<f.length;i++){
-        for(j=0:j<m.length;j++){
+        for(j=0;j<m.length;j++){
             if(m[i]%f[j]==0){
                 sum+=m[i];}
-
-        }
+            }
         
         }
     }
